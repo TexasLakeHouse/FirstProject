aac75eda3729499f965df6e2622f0d5588480743
TexasLakeHouse <deanbortell@gmail.com>
2015-01-11 16:34:15 -0600

    Checked in label update to test github.

--- FirstGithub/Form1.Designer.cs
+++ FirstGithub/Form1.Designer.cs
@@ -36,15 +36,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(203, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "First GitHub Project";
+            this.label1.Text = "First GitHub Project Updated for Check In";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(172, 61);
+            this.ClientSize = new System.Drawing.Size(275, 113);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.ResumeLayout(false);
